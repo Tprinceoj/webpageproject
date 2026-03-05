c8dc7afd8357819ed91032327b8729a05dc04ba1
Tprinceoj <Tprinceoj@gmail.com>
2026-03-05 17:30:13 +0100

    fist comit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    alert("Photography allows me to capture the beauty of the world and express my creativity!");
+}
+document.getElementById("infoButton").addEventListener("click", showMessage);
