757c41fc8776bf6da3bb927ae5b322153d076e41
Tprinceoj <Tprinceoj@gmail.com>
2026-03-12 16:50:28 +0100

    second commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,36 @@
-function showMessage() {
-    alert("Photography allows me to capture the beauty of the world and express my creativity!");
+const taskInput = document.getElementById('task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskList = document.getElementById('task-list');  
+
+addTaskBtn.addEventListener('click', addTask) 
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const taskListItem = document.createElement('li');
+        taskListItem.textContent = taskText;
+        const removeTaskBtn = document.createElement('button');     
+        removeTaskBtn.textContent = 'Remove';
+        removeTaskBtn.addEventListener('click', removeTask);
+        const completeTaskCheckbox = document.createElement('input');
+        completeTaskCheckbox.type = 'checkbox';
+        completeTaskCheckbox.addEventListener('change', completeTask);
+        taskListItem.appendChild(completeTaskCheckbox);
+        taskListItem.appendChild(removeTaskBtn);
+        taskList.appendChild(taskListItem);
+        taskInput.value = '';
+    }
 }
-document.getElementById("infoButton").addEventListener("click", showMessage);
+
+function removeTask(event) {
+    const taskListItem = event.target.parentNode;
+    taskList.removeChild(taskListItem);
+}
+function completeTask(event) {
+    const taskListItem = event.target.parentNode;
+    if (event.target.checked) {
+        taskListItem.style.textDecoration = 'line-through';
+    } else {
+        taskListItem.style.textDecoration = 'none';
+    }
+}
